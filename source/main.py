edf047fe2ccb40c5892362e7e99199d307127c49
kerem.ea <krembertaktas@gmail.com>
2025-09-03 20:10:03 +0200

    trying again

--- source/main.py
+++ source/main.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+   page_title="Main",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
 st.title("Kerem's app") #st.title creates a title, for the current page. It can be any datatype. Parameters: st.title(body, anchor=None, *, help=None, width="stretch")
 
 st.badge("Test", color="violet") # Badge creates a tag. Parameters: st.badge(label, *, icon=None, color="blue", width="content")
--- source/other.py
+++ source/other.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+   page_title="Gigachad",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
 st.title("Gigachad.")
 st.badge("Gigachad", color="red")
